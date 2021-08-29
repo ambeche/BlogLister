e39784426de6afd78bf53e025a72939c94127758
tamanjic <tamanji.che@metropolia.fi>
2021-08-29 19:18:06 +0300

    bug fix attempt: some styles/className of mui styles not working in pdt build

--- src/styles/useStyles.js
+++ src/styles/useStyles.js
@@ -13,101 +13,106 @@ export const theme = createTheme({
   }
 });
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    marginBottom: '3%',
-    flexGrow: 1
-  },
-  desktopView: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex'
-    }
-  },
-  appBarEnd: {
-    [theme.breakpoints.between('md', 'lg')]: { marginLeft: theme.spacing(40) },
-    [theme.breakpoints.up('lg')]: { marginLeft: theme.spacing(80) }
-  },
-  appBarTab: {
-    backgroundColor: theme.palette.primary.light,
-    boxShadow: `4px 4px 1px 1px ${theme.palette.secondary.light}`,
-    [theme.breakpoints.up('md')]: {
-      backgroundColor: theme.palette.secondary.dark,
-      boxShadow: '0 0 0 0'
-    }
-  },
-  appName: {
-    [theme.breakpoints.up('md')]: {
-      marginRight: theme.spacing(6),
-      marginLeft: theme.spacing(1.5)
+const useStyles = makeStyles(
+  (theme) => ({
+    appBar: {
+      marginBottom: '3%',
+      flexGrow: 1
     },
-    [theme.breakpoints.only('sm')]: {
-      marginLeft: '55%'
+    desktopView: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex'
+      }
     },
-    [theme.breakpoints.only('xs')]: {
-      marginLeft: '30%'
+    appBarEnd: {
+      [theme.breakpoints.between('md', 'lg')]: {
+        marginLeft: theme.spacing(40)
+      },
+      [theme.breakpoints.up('lg')]: { marginLeft: theme.spacing(80) }
     },
-    textDecoration: 'none'
-  },
-  menuIcon: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none'
-    }
-  },
-  menuItemProfile: {
-    marginRight: theme.spacing(0)
-  },
-  formSubmitBtn: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(0.5)
-  },
-  formTitle: {
-    marginBottom: theme.spacing(2)
-  },
-  commentForm: {
-    marginBottom: theme.spacing(2),
-    marginTop: theme.spacing(2),
-    padding: theme.spacing(2),
-    '& button': {
-      marginTop: theme.spacing(1),
-      marginLeft: theme.spacing(1)
-    }
-  },
-  listSecondaryActionsDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'flex',
+    appBarTab: {
+      backgroundColor: theme.palette.primary.light,
+      boxShadow: `4px 4px 1px 1px ${theme.palette.secondary.light}`,
+      [theme.breakpoints.up('md')]: {
+        backgroundColor: theme.palette.secondary.dark,
+        boxShadow: '0 0 0 0'
+      }
+    },
+    appName: {
+      [theme.breakpoints.up('md')]: {
+        marginRight: theme.spacing(6),
+        marginLeft: theme.spacing(1.5)
+      },
+      [theme.breakpoints.only('sm')]: {
+        marginLeft: '55%'
+      },
+      [theme.breakpoints.only('xs')]: {
+        marginLeft: '30%'
+      },
+      textDecoration: 'none'
+    },
+    menuIcon: {
+      [theme.breakpoints.up('md')]: {
+        display: 'none'
+      }
+    },
+    menuItemProfile: {
+      marginRight: theme.spacing(0)
+    },
+    formSubmitBtn: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(0.5)
+    },
+    formTitle: {
+      marginBottom: theme.spacing(2)
+    },
+    commentForm: {
+      marginBottom: theme.spacing(2),
+      marginTop: theme.spacing(2),
+      padding: theme.spacing(2),
+      '& button': {
+        marginTop: theme.spacing(1),
+        marginLeft: theme.spacing(1)
+      }
+    },
+    listSecondaryActionsDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'flex',
+        padding: theme.spacing(1),
+        '& button': {
+          marginLeft: theme.spacing(2)
+        }
+      }
+    },
+    listSecondaryActionsMobile: {
+      [theme.breakpoints.up('sm')]: {
+        display: 'none'
+      },
       padding: theme.spacing(1),
       '& button': {
-        marginLeft: theme.spacing(2)
+        marginLeft: theme.spacing(4)
       }
-    }
-  },
-  listSecondaryActionsMobile: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'none'
     },
-    padding: theme.spacing(1),
-    '& button': {
-      marginLeft: theme.spacing(4)
+    listItemPrimary: {
+      maxWidth: theme.spacing(35)
+    },
+    profile: {
+      '& h6': {
+        marginTop: '4%'
+      }
+    },
+    userDetails: {
+      '& h6': {
+        marginTop: '2%'
+      }
+    },
+    formsContainerComponent: {
+      marginBottom: '3%'
     }
-  },
-  listItemPrimary: {
-    maxWidth: theme.spacing(35)
-  },
-  profile: {
-    '& h6': {
-      marginTop: '4%'
-    }
-  },
-  userDetails: {
-    '& h6': {
-      marginTop: '2%'
-    }
-  },
-  formsContainerComponent: {
-    marginBottom: '3%'
-  }
-}));
+  }),
+  { index: 1 }
+);
 
 export default useStyles;
